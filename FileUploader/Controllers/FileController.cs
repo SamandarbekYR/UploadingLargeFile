45ce85e3ed90be95837c5981213b773a284a6f31
SamandarbekYr <SamandarbekYR@gmail.com>
2023-12-14 00:16:40 +0500

    Add project files.

---
+++ FileUploader/Controllers/FileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
+
+namespace FileUploader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpPost]
+      //  [RequestSizeLimit(700* 1024 * 1024)]
+        public IActionResult File(IFormFile file)
+        {
+            var fileMb = file.Length / (1024 * 1024);
+
+            // Assuming you want to return the file size as a string
+            return Content($"{fileMb} MB");
+        }
+
+    }
+}
---
+++ FileUploader/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Http.Features;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MaxRequestBodySize = 700 * 1024 * 1024;
+});
+
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 700 * 1024 * 1024;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
